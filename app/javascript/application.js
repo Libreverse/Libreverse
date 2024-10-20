23fd134daf666e0c33f129901900cabf3f36dfb7
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2024-10-20 15:06:44 +0100

    migrated to postgres and safety commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,3 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import $ from 'jquery';
-import 'what-input';
-import 'motion-ui';
-import * as Foundation from 'foundation-sites';
-window.$ = window.jQuery = $;
-document.addEventListener('DOMContentLoaded', () => {
-  $(document).foundation();
-});
 import "./controllers"
