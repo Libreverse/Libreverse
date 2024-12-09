8ec9e3eb1c5be5962dbee781fef559b3df113cb4
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2024-12-09 16:40:08 +0000

    readd action mailer in prep for mailgun integration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,4 +65,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Action mailer configuration
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
 end
