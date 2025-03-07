380f14d41414e3709a769312c0ea8a66e6d808c5
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2025-03-07 17:50:52 +0000

    small improvements to the drawer and some configs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,8 @@
-import * as Sentry from "@sentry/browser";
+/* import * as Sentry from "@sentry/browser";
 
 Sentry.init({
-  dsn: "https://464a84695ee24afbb22817b94618d577@glitchtip-cs40w800ggw0gs0k804skcc0.geor.me/5",
-  tracesSampleRate: 1,
-});
+  dsn: "",
+}); */
 
 import "@hotwired/turbo-rails";
 
