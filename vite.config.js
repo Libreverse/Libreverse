0fb5eff7c30a8beedab1ca81c3ec6eae0b8ea4fb
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2025-04-16 11:12:20 +0100

    add inline dynamic imports to vite

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig(({ mode }) => {
     return {
         build: {
             sourcemap: false,
+            inlineDynamicImports: true,
             cache: !isDevelopment,
             rollupOptions: {
                 output: {
