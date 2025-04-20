b255eb9aab5d92fab53236563658fa1179e42eb6
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2025-04-20 14:04:24 +0100

    Add dynamic data cleaners for GDPR

--- db/migrate/20250412120542_drop_sessions_table.rb
+++ db/migrate/20250412120542_drop_sessions_table.rb
@@ -4,8 +4,10 @@ class DropSessionsTable < ActiveRecord::Migration[8.0]
   def change
     reversible do |dir|
       dir.up do
-        # Explicitly drop the table when migrating up
-        drop_table :sessions, if_exists: true # Add if_exists for safety
+        # Explicitly drop unused session tables
+        %i[sessions sessions_table_for_active_record_stores].each do |tbl|
+          drop_table tbl, if_exists: true
+        end
       end
       dir.down do
         # This migration is intended to drop the table.
