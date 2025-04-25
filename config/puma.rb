f898758a461468d15c1c7f5f93c130c97e36585d
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2025-04-25 21:26:20 +0100

    this *should* fix it

--- config/puma.rb
+++ config/puma.rb
@@ -23,7 +23,7 @@ environment(ENV.fetch("RAILS_ENV") { "development" })
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
-workers 1
+workers 0
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
