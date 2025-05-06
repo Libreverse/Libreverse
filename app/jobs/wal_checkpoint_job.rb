2642e0f2e40c11a32908d4f21ce3e77bf86ea5c1
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2025-05-06 19:20:56 +0100

    some more perf tweaks

--- app/jobs/wal_checkpoint_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class WalCheckpointJob < ApplicationJob
-  queue_as :maintenance
-
-  def perform
-    ActiveRecord::Base.connection.execute("PRAGMA wal_checkpoint('TRUNCATE');")
-  end
-end
