7f2fbbc2a3f039c64a012be83006bace4963e7c6
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2025-06-16 11:11:48 +0100

    fix broken migration

--- db/migrate/20250613134012_create_federails_moderation_reports.federails_moderation.rb
+++ db/migrate/20250613134012_create_federails_moderation_reports.federails_moderation.rb
@@ -6,7 +6,6 @@ class CreateFederailsModerationReports < ActiveRecord::Migration[7.0]
     create_table :federails_moderation_reports do |t|
       t.string :federated_url
       t.references :federails_actor, foreign_key: true
-      t.string :content
       t.references :object, polymorphic: true
       t.datetime :resolved_at
       t.text   :content
