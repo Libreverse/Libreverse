b79254045c139b7ccbb24b61647a42dd6bd8268d
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2025-06-20 11:23:27 +0100

    fix stylelint failure

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable unicorn/no-null */
 export default {
     extends: ["stylelint-config-standard-scss"],
-    ignoreFiles: ["**/tailwind.scss", "**/application.scss"],
+    ignoreFiles: ["**/tailwind.scss", "**/application.scss", "**/gems/**", "vendor/**"],
     rules: {
         "no-empty-source": undefined,
     },
