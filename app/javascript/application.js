0160a97b158b57f56e29fbcf3c60683a786dc748
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2025-07-12 20:41:31 +0100

    crank up terser power now we know the source of the crash

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 import "../stylesheets/application.scss";
-import sxwjs from "@sxwjs/sxwjs";
 import html2canvas from "html2canvas";
 import debounced from "debounced";
 import "./libs/foundation.js";
@@ -28,49 +27,6 @@ debounced.register(["resize"], {
 
 // Make html2canvas globally available for liquid glass effects
 globalThis.html2canvas = html2canvas;
-// Custom configuration
-const myConfig = {
-    stopColor: "red",
-    stopFontWeight: "bold",
-    cautionFontWeight: "bold",
-    cautionFontSize: "15px",
-};
-sxwjs.setConfig(myConfig);
-
-// Custom content
-const myContent = {
-    en: {
-        stopText: `            uuuuuuuuuuuuuuuuuuuu
-          u" uuuuuuuuuuuuuuuuuu "u
-        u" u$$$$$$$$$$$$$$$$$$$$u "u
-      u" u$$$$$$$$$$$$$$$$$$$$$$$$u "u
-    u" u$$$$$$$$$$$$$$$$$$$$$$$$$$$$u "u
-  u" u$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$u "u
-u" u$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$u "u
-$ $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ $
-$ $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ $
-$ $$$" ... "$...  ...$" ... "$$$  ... "$$$ $
-$ $$$u \`"$$$$$$$  $$$  $$$$$  $$  $$$  $$$ $
-$ $$$$$$uu "$$$$  $$$  $$$$$  $$  """ u$$$ $
-$ $$$""$$$  $$$$  $$$u "$$$" u$$  $$$$$$$$ $
-$ $$$$....,$$$$$..$$$$$....,$$$$..$$$$$$$$ $
-$ $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ $
-"u "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" u"
-  "u "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" u"
-    "u "$$$$$$$$$$$$$$$$$$$$$$$$$$$$" u"
-      "u "$$$$$$$$$$$$$$$$$$$$$$$$" u"
-        "u "$$$$$$$$$$$$$$$$$$$$" u"
-          "u """""""""""""""""" u"
-            """"""""""""""""""""`,
-        cautionText: "IMPORTANT SECURITY WARNING ⬇",
-        warningText:
-            "This is a tool for web developers only.\n\nAnything entered here is code that will be run on your computer.\n\nSomeone may have told you to paste something here and press enter.\n\nTHIS IS A COMMON SCAM.\n\nAnyone who tells you to ignore this warning is trying to hack your account, no matter who you think they are.",
-    },
-};
-sxwjs.setContent(myContent);
-
-// Print the customized warning
-sxwjs.printWarning("en");
 
 import * as Turbo from "@hotwired/turbo";
 import TurboPower from "turbo_power";
