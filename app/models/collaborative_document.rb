eb5a8566da16b51fd8560201cad79ce379e9c9ee
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2025-08-19 22:39:23 +0100

    cache optimisations and general fixes

--- app/models/collaborative_document.rb
+++ app/models/collaborative_document.rb
@@ -5,6 +5,17 @@ class CollaborativeDocument < ApplicationRecord
 
   validates :session_id, presence: true, uniqueness: true
 
+  after_initialize do
+    # Provide in-Ruby defaults since MySQL/TiDB disallow defaults on BLOB/JSON.
+    self.base_snapshot ||= "".b
+    self.version_vector ||= {}
+  end
+
+  before_validation do
+    self.base_snapshot = "".b if base_snapshot.nil?
+    self.version_vector = {} if version_vector.nil?
+  end
+
   # Append a Yjs update, auto-incrementing seq
   def append_update!(raw_bytes)
     next_seq = (collaborative_document_updates.maximum(:seq) || 0) + 1
--- app/models/session_finalization.rb
+++ app/models/session_finalization.rb
@@ -2,4 +2,14 @@
 
 class SessionFinalization < ApplicationRecord
   validates :session_id, presence: true, uniqueness: true
+
+  after_initialize do
+    self.transient_state ||= {}
+    self.yjs_vector ||= {}
+  end
+
+  before_validation do
+    self.transient_state = {} if transient_state.nil?
+    self.yjs_vector = {} if yjs_vector.nil?
+  end
 end
