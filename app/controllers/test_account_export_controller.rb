089d1af55149e52fd0ab11bdca69812f24257c2a
georgebaskervil <148500997+georgebaskervil@users.noreply.github.com>
2025-09-21 12:22:45 +0000

    chore(autofix): apply automated format/lint fixes

--- app/controllers/test_account_export_controller.rb
+++ app/controllers/test_account_export_controller.rb
@@ -4,7 +4,7 @@
 # Test-only minimal implementation of account export to isolate ZIP streaming
 # from authentication, Rodauth constraints, and Vite asset compilation. Loaded
 # only in test environment; production uses AccountActionsController.
-class TestAccountExportController < ActionController::Base
+class TestAccountExportController < ApplicationController
   include ZipKit::RailsStreaming
   layout false
 
@@ -12,7 +12,7 @@ class TestAccountExportController < ActionController::Base
     zip_kit_stream(filename: "libreverse_export.zip", type: "application/zip") do |zip|
       zip.write_deflated_file("account.xml") { |s| s << '<account username="test" />' }
     end
-    response.headers['X-Accel-Buffering'] = 'no'
-    response.headers['Content-Encoding'] = 'identity'
+    response.headers["X-Accel-Buffering"] = "no"
+    response.headers["Content-Encoding"] = "identity"
   end
 end
