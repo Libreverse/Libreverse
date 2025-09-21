dac9c4ae7d93f06314853bfe8cb0fa8a7925814d
georgebaskervil <148500997+georgebaskervil@users.noreply.github.com>
2025-09-21 18:19:18 +0000

    chore(autofix): apply automated format/lint fixes

--- app/javascript/controllers/comment_bulk_controller.js
+++ app/javascript/controllers/comment_bulk_controller.js
@@ -4,9 +4,10 @@ export default class extends Controller {
     static targets = [];
     toggleAll(event) {
         const checked = event.target.checked;
-        for (const callback of this.element
-            .querySelectorAll("tbody input[type=checkbox]")) {
-                callback.checked = checked;
-            }
+        for (const callback of this.element.querySelectorAll(
+            "tbody input[type=checkbox]",
+        )) {
+            callback.checked = checked;
+        }
     }
 }
