e238654d28a0e8b1454835a8dd9f70e6d9eed9ae
George Baskerville <148500997+georgebaskervil@users.noreply.github.com>
2025-09-26 17:21:31 +0100

    safety

--- Procfile
+++ Procfile
@@ -1,7 +1,7 @@
 web: bundle exec passenger start -a 127.0.0.1 -p 3000 --environment development --max-pool-size=2
 vite: bin/vite dev
-webpack-client: WEBPACK_SERVE=true bin/shakapacker-dev-server
+webpack-client: bin/shakapacker-dev-server
+shakapacker-hack: bin/shakapacker-hack
 webpack-server: SERVER_BUNDLE_ONLY=yes bin/shakapacker --watch
 jobs: bundle exec bin/jobs
 mailhog: mailhog
-docsify: docsify serve ./documentation -p 4000
--- config/webpack/development.cjs
+++ config/webpack/development.cjs
@@ -1,10 +1,3 @@
 const generateWebpackConfigs = require("./generateWebpackConfigs.cjs");
 
-const developmentEnvironmentOnly = (clientWebpackConfig) => {
-    if (process.env.WEBPACK_SERVE) {
-        const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
-        clientWebpackConfig.plugins.push(new ReactRefreshWebpackPlugin({}));
-    }
-};
-
-module.exports = generateWebpackConfigs(developmentEnvironmentOnly);
+module.exports = generateWebpackConfigs();
